bad8794f9169541eb4a57b90c922b3587390ab97
Marc Simon <marc.simon42@gmail.com>
2021-03-16 13:01:58 +0000

    Search for the correct event in the logs to be more resistant to contract changes

--- auto_harvest.py
+++ auto_harvest.py
@@ -8,6 +8,8 @@ from pydash import _
 from time import time
 from datetime import datetime
 
+ADDR_SIZE = 66
+ROOT_ADDR = "0x0000000000000000000000000000000000000000000000000000000000000000"
 AUTO_CONTRACT = "0x0895196562C7868C5Be92459FaE7f877ED450452"
 AUTO_ABI = '[{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":true,"internalType":"uint256","name":"pid","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":true,"internalType":"uint256","name":"pid","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"EmergencyWithdraw","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":true,"internalType":"uint256","name":"pid","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Withdraw","type":"event"},{"inputs":[],"name":"AUTO","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"AUTOMaxSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"AUTOPerBlock","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"AUTOv2","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_allocPoint","type":"uint256"},{"internalType":"contract IERC20","name":"_want","type":"address"},{"internalType":"bool","name":"_withUpdate","type":"bool"},{"internalType":"address","name":"_strat","type":"address"}],"name":"add","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"burnAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"},{"internalType":"uint256","name":"_wantAmt","type":"uint256"}],"name":"deposit","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"}],"name":"emergencyWithdraw","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_from","type":"uint256"},{"internalType":"uint256","name":"_to","type":"uint256"}],"name":"getMultiplier","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_token","type":"address"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"inCaseTokensGetStuck","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"massUpdatePools","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_inputAmt","type":"uint256"}],"name":"migrateToAUTOv2","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"ownerAUTOReward","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"},{"internalType":"address","name":"_user","type":"address"}],"name":"pendingAUTO","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"poolInfo","outputs":[{"internalType":"contract IERC20","name":"want","type":"address"},{"internalType":"uint256","name":"allocPoint","type":"uint256"},{"internalType":"uint256","name":"lastRewardBlock","type":"uint256"},{"internalType":"uint256","name":"accAUTOPerShare","type":"uint256"},{"internalType":"address","name":"strat","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"poolLength","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"},{"internalType":"uint256","name":"_allocPoint","type":"uint256"},{"internalType":"bool","name":"_withUpdate","type":"bool"}],"name":"set","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"},{"internalType":"address","name":"_user","type":"address"}],"name":"stakedWantTokens","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"startBlock","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalAllocPoint","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"}],"name":"updatePool","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"address","name":"","type":"address"}],"name":"userInfo","outputs":[{"internalType":"uint256","name":"shares","type":"uint256"},{"internalType":"uint256","name":"rewardDebt","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"},{"internalType":"uint256","name":"_wantAmt","type":"uint256"}],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"}],"name":"withdrawAll","outputs":[],"stateMutability":"nonpayable","type":"function"}]'
 
@@ -55,8 +57,7 @@ def auto_compund(wallet_address, private_key, min_amount_to_harvest):
         current_pending_auto_eth = w3.fromWei(current_pending_auto_gwei, 'ether')
 
         if current_pending_auto_eth * auto_price_usd > min_amount_to_harvest:
-            print("\n")
-            print(f"- Current pending auto for pool id {pool_id}: {current_pending_auto_eth} = ${current_pending_auto_eth * auto_price_usd:.2f}. Withdrawing...")
+            print(f"- Current pending auto for pool id {pool_id}: {current_pending_auto_eth} = ${current_pending_auto_eth * auto_price_usd:.2f}. Withdrawing...", end='')
             # withdrawing a pool with 0 only withdraw the reward = Harvest
             tx = auto_contract.functions.withdraw(pool_id, 0).buildTransaction({
                 'from': wallet_address,
@@ -108,8 +109,13 @@ def auto_compund(wallet_address, private_key, min_amount_to_harvest):
             tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
             print(f"...done.\n tx hash = {tx_receipt['transactionHash'].hex()}")
 
+            transfer_log = _.find(tx_receipt['logs'], lambda x: x['address'] == WRAP_BNB_TOKEN_CONTRACT)
+            if not transfer_log or not transfer_log.get('data'):
+                print(f"Error: Could not find log for the WBNB transfer. Transaction must have failed.")
+                exit(-1)
+
             ## ADD LIQUIDITY TO PANCAKE SWAP. SWAP AUTO & WBNB FOR WBNB-AUTO LP TOKEN
-            received_wbnb_amt_gwei = int(_.get(tx_receipt, 'logs[2].data'), 16)
+            received_wbnb_amt_gwei = int(transfer_log['data'], 16)
 
             wbnb_amt_gwei = int(received_wbnb_amt_gwei * (1 - PANCAKE_ASSUMED_SLIPPAGE))
             print(f"- Add lidquidity AUTO-WBNB: AUTO {half_harvested_auto_amt_gwei} for WBNB {wbnb_amt_gwei} on pancakeswap...", end='')
@@ -133,8 +139,22 @@ def auto_compund(wallet_address, private_key, min_amount_to_harvest):
             tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
             print(f"...done.\n tx hash = {tx_receipt['transactionHash'].hex()}")
 
-            ## DEPOSITE LP TOKEN TO AUTOFARM WBNB-AUTO POOL
-            liquidity_created = int(_.get(tx_receipt, 'logs[4].data'), 16)
+            def find_transfer_lp_logs(log):
+                # Search for the log transfering money to your wallet
+                # Transfer log have 3 topics: function hash, from, to.
+                # Looking for `from = 0x0` & `to = wallet_address`
+                topics = log['topics']
+                if len(topics) == 3 and topics[1].hex() == ROOT_ADDR:
+                    # extract the leading 0 from the address in the log
+                    addr = '0x' + topics[2].hex()[ADDR_SIZE-len(wallet_address)+2:]
+                    return addr == wallet_address.lower()
+
+            transfer_lp_log = _.find(tx_receipt['logs'], find_transfer_lp_logs)
+            if not transfer_lp_log or not transfer_lp_log.get('data'):
+                print(f"Error: Could not find log for the LP transfer. Transaction must have failed.")
+                exit(-1)
+
+            liquidity_created = int(transfer_lp_log['data'], 16)
 
             print(f"- Add {w3.fromWei(liquidity_created, 'ether')} token back into AUTO-WBNB LP vault...", end='')
             tx = auto_contract.functions.deposit(AUTO_WBNB_POOL_ID, liquidity_created).buildTransaction({
